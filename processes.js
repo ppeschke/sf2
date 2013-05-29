84c6d561e5573fa72aa80ed741662da98ab8bba9
Phillip Peschke <phillipdpeschke@gmail.com>
2013-05-29 01:19:58 -0500

    Made progress on debug log prototype
    - Changed debugLog.html to respond to a .js file and fill in the document
      accordingly
    - Added processes.js, which the DebugTools class will write

---
+++ processes.js
@@ -0,0 +1,16 @@
+Processes.push(new Process("Frame 14", "Frame 14", "framescontainer", 31, true));
+Processes.push(new Process("Frame 14 Process", "Process", "Frame 14", 17, false));
+Processes.push(new Process("Frame 14 Process Run", "Run", "Frame 14 Process", 4, false));
+Processes.push(new Process("Frame 14 Process End Step", "End Step", "Frame 14 Process", 7, false));
+Processes.push(new Process("Frame 14 Process Collision", "Collision", "Frame 14 Process", 0, false));
+Processes.push(new Process("Frame 14 Process Reconcile", "Reconcile", "Frame 14 Process", 6, false));
+Processes.push(new Process("Frame 14 Render", "Render", "Frame 14", 15, false));
+Processes.push(new Process("Frame 14 Render Draw Objects", "Draw Objects", "Frame 14 Render", 14, false));
+/*Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));
+Processes.push(new Process("", "", "", , false));*/
