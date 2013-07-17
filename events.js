1a6cecf2ae955761e90296a331ba4e65d9b39a51
Phillip Peschke <phillipdpeschke@gmail.com>
2013-07-17 10:34:48 -0500

    Removed js files from git tracking because they don't need to be tracked.

--- events.js
+++
@@ -1,5 +0,0 @@
-_Events.push(new Event("Game mode changed to settings", 118));
-_Events.push(new Event("Game mode changed to running", 166));
-_Events.push(new Event("Game mode changed to choosingShip", 166));
-_Events.push(new Event("Game mode changed to running", 213));
-_Events.push(new Event("Game mode changed to over", 832));
